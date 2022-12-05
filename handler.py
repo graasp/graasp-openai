323974c461067429ce8245a1755c75cbd0913710
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2022-12-05 14:12:08 +0100

    fix: fix linting errors

--- handler.py
+++ handler.py
@@ -1,12 +1,11 @@
+import os
 import json
 import logging
 import openai
-import os
-
 
 # get environment variables
 DEBUG = os.getenv("DEBUG", False)
-OPENAI_API_KEY= os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # set openai api key
 openai.api_key = OPENAI_API_KEY
@@ -21,14 +20,14 @@ if DEBUG:
 # serverless api python function that makes a call to the openai completion api and returns its output
 def generate(event, context):
     log.debug("Received event {}".format(json.dumps(event)))
- 
+
     # default to no prompt
     prompt = None
 
     # check if body exists and if it contains a prompt
     if 'body' in event:
         body = json.loads(event['body'])
-       # if prompt exists assign it to variable
+        # if prompt exists assign it to variable
         if 'prompt' in body:
             prompt = body['prompt']
 
@@ -36,18 +35,16 @@ def generate(event, context):
     if prompt is None:
         return {
             'statusCode': 400,
-            'body': json.dumps({ 'message': 'prompt is undefined' })
+            'body': json.dumps({'message': 'prompt is undefined'})
         }
 
-    response = openai.Completion.create(
-        model="code-davinci-002",
-        prompt=prompt,
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        presence_penalty=0.6,
-        stop=["Student:"]
-    )
+    response = openai.Completion.create(model="code-davinci-002",
+                                        prompt=prompt,
+                                        temperature=0.9,
+                                        max_tokens=150,
+                                        top_p=1,
+                                        presence_penalty=0.6,
+                                        stop=["Student:"])
 
     # return the response
     return {
@@ -56,6 +53,5 @@ def generate(event, context):
             'Access-Control-Allow-Credentials': True,
         },
         'statusCode': 200,
-        'body': json.dumps({ 'completion': response['choices'][0]['text'] })
+        'body': json.dumps({'completion': response['choices'][0]['text']})
     }
-
